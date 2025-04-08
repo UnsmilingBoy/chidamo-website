ec10beca82651b8454d6534fbd66dc257b76b86b
Sepanta <127672679+UnsmilingBoy@users.noreply.github.com>
2025-04-08 22:22:08 +0330

    Banner updated for mobile

--- components/BannerSlider.jsx
+++ components/BannerSlider.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function BannerSlider() {
   const [isMobile, setIsMobile] = useState(false);
@@ -18,14 +19,13 @@ export default function BannerSlider() {
     return () => window.removeEventListener("resize", checkScreenSize);
   }, []);
   const banners = [
-    "/images/banner-1.png",
-    "/images/banner-2.png",
-    "/images/banner-3.png",
+    ["/images/banner-1.png", "/category/90"],
+    ["/images/banner-2.png", "/category/88"],
+    ["/images/banner-3.png", "/category/90"],
   ];
   const bannersMobile = [
-    "/images/banner-4-mobile.png",
-    "/images/banner-4-mobile.png",
-    "/images/banner-4-mobile.png",
+    ["/images/banner-4-mobile.png"],
+    ["/images/banner-2-mobile.png", "/category/90"],
   ];
 
   const settings = {
@@ -46,13 +46,15 @@ export default function BannerSlider() {
     >
       {isMobile
         ? bannersMobile.map((banner, index) => (
+            // <Link href={banner[1]} key={index}>
             <div key={index}>
-              <img src={banner} alt={"Banner number" + index} />
+              <img src={banner[0]} alt={"Banner number" + index} />
             </div>
+            // </Link>
           ))
         : banners.map((banner, index) => (
             <div key={index}>
-              <img src={banner} alt={"Banner number" + index} />
+              <img src={banner[0]} alt={"Banner number" + index} />
             </div>
           ))}
     </Slider>
